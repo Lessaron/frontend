c85d6d8b4d0ecbcfd2c608e230c9c3447eaa10c2
Alessandra Rondini <lessarondini@gmail.com>
2025-11-11 16:53:40 -0300

    Ajuste css

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,7 @@ import {Link } from "react-router-dom";
 export default function Navbar() {
     return (
         <nav className="navbar h-12">
-        <h1 className="navbar-title">Cadastro de Clientes</h1>
+        <h1 className="navbar-title">Mara Rondini Podóloga</h1>
         <div>
           <Link to="/" className="nav-link">Home</Link>
           <Link to="/cliente"    className="nav-link">Cadastro</Link>
